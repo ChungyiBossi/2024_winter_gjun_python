832c7c445ba54af6182526382cb25fa3526da21b
ChungyiBossi <chungyi.bossi@gmail.com>
2025-01-02 19:46:00 +0800

    [feature] news prototype

--- google_news.py
+++ google_news.py
@@ -15,7 +15,17 @@ topic_elements = soup.find_all("div", class_="W8yrY")
 print(len(topic_elements))
 
 for topic_element in topic_elements:
-    left_side_a = topic_element.find('a', class_="gPFEn")
-    # print(left_side_a, '\n')
-    print(left_side_a.text)  # 新聞標題
-    print(left_side_a['href'], '\n')  # 　原新聞連結
+    # 新聞標題
+    news_titles = topic_element.find_all('a', class_="gPFEn")
+    # 新聞媒體
+    medias = topic_element.find_all('div', class_='vr1PYe')
+    # 發布時間
+    public_times = topic_element.find_all('time', class_='hvbAAd')
+
+    for media, news_title, public_time in zip(medias, news_titles, public_times):
+        print(media.text)
+        print(news_title.text)  # 新聞標題
+        print(public_time['datetime'])
+        print()
+
+    # break
