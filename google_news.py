e8d3ea87e857ee3d0c834e081a44a8a3e4e9dcb7
ChungyiBossi <chungyi.bossi@gmail.com>
2025-01-02 21:18:30 +0800

    [feature] news to csv

--- google_news.py
+++ google_news.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # step 1 : request webpage html
 url = "https://news.google.com/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNRFptTXpJU0JYcG9MVlJYS0FBUAE?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant"
@@ -14,7 +15,8 @@ soup = BeautifulSoup(response.text, 'html.parser')
 topic_elements = soup.find_all("div", class_="W8yrY")
 print(len(topic_elements))
 
-for topic_element in topic_elements:
+list_of_news = list()
+for topic_id, topic_element in enumerate(topic_elements):
     # 新聞標題
     news_titles = topic_element.find_all('a', class_="gPFEn")
     # 新聞媒體
@@ -23,9 +25,22 @@ for topic_element in topic_elements:
     public_times = topic_element.find_all('time', class_='hvbAAd')
 
     for media, news_title, public_time in zip(medias, news_titles, public_times):
-        print(media.text)
-        print(news_title.text)  # 新聞標題
-        print(public_time['datetime'])
-        print()
+        # print(media.text)
+        # print(news_title.text)  # 新聞標題
+        # print(public_time['datetime'])
+        # print()
+        list_of_news.append(
+            {
+                'topic_id': topic_id,
+                'media_name': media.text,
+                'title': news_title.text,
+                'url': 'https://news.google.com' + news_title['href'][1:],
+                'public_time': public_time['datetime']
+            }
+        )
 
-    # break
+
+print(len(list_of_news))
+
+df = pd.DataFrame(list_of_news)
+df.to_csv("news.csv")
